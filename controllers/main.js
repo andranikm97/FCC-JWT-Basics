db55ac615954fbfd3d162efcd168d32657dd67f9
Andranik Markaryan <andranik.mrk@gmail.com>
2021-12-13 17:32:00 +0300

    Feat: implement authentication middleware

--- controllers/main.js
+++ controllers/main.js
@@ -19,31 +19,17 @@ const login = async (req, res) => {
       username,
     },
     process.env.JWT_SECRET,
-    { expiresIn: '30d' },
+    { expiresIn: '30 minutes' },
   );
-  console.log(username, password);
   res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('User must have a token', 401);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Your authorized data --> lucky number: ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route', 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Your authorized data --> lucky number: ${luckyNumber}`,
+  });
 };
 
 module.exports = {
